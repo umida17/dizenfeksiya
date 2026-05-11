4aeff13b1a4c48796f39cec86447ed6d1e77a251
uy <a.normurzaev20@gmail.com>
2026-05-11 23:22:09 +0500

    dizenfeksiya bowlandi

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,5 +1,6 @@
 
 import "./src/Header/Header.css"
+import frame from './assets/Frame.png'
 
 import React from 'react'
 
@@ -9,7 +10,7 @@ const Header = () => {
     <header>
         <div className="container">
             <div className="herader-container">
-                
+            <img src={frame} alt="" />
             </div>
         </div>
     </header>
