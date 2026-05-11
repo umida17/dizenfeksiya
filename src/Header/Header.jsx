d2abab5746ab6cc2cf8bfe171efa4e1684464684
uy <a.normurzaev20@gmail.com>
2026-05-11 23:30:10 +0500

    dizenfeksiya bowlandi header qismi

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -10,6 +10,14 @@ const Header = () => {
         
         <div className="header-container">
            <img src={frame} alt="Logo" />
+           <div className="home">
+            <a href="#">Service</a>
+            <a href="#">About</a>
+            <a href="#">Faq</a>
+            <a href="#">Contact</a>
+           </div>
+
+           <button>Kirish</button>
         </div>
       </div>
     </header>
