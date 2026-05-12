6053db59d14c37861f686651e61648b924b73a48
uy <a.normuraev20@gmail.com>
2026-05-12 23:35:42 +0500

    hero qismi bowlandi stil

--- src/Header/Hero/Hero.jsx
+++ src/Header/Hero/Hero.jsx
@@ -12,18 +12,18 @@ const Hero = () => {
         <div className="Hero-container">
             <div className="dizenfeksiya">
                 <h2 className='xizmat'>Dizenfeksiya xizmati </h2>
-                <p>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p>
+                <p className='kop'>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p>
                 <div className="dumaloq">
                     <div className="kok"></div>
                     <div className="mentol"></div>
-                    <img src={group} alt="" />
+                    <img className='siyoh' src={group} alt="" />
                 </div>
                 <div className="boshlanish">
                     <button>Bog’lanish</button>
                         <img src={vector} alt="" />
                 </div>
             </div>
-            <img src={xlor} alt="" />
+            <img className='xlor' src={xlor} alt="" />
         </div>
     </div>
      </div>
