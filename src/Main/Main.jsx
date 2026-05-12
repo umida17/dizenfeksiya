929ed7a9ea0ab37efb56de2cdd01c6f13d064b14
uy <a.normuraev20@gmail.com>
2026-05-13 00:09:40 +0500

    main jsx yakunlandi

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Main.css"
+import mask from  '../assets/Mask.png'
 
 const Main = () => {
   return (
@@ -7,7 +8,31 @@ const Main = () => {
        <div className="section1">
           <div className="container">
              <div className="section1-containeri">
-               
+               <h2>Avfzalliklarimiz</h2>
+               <div className="cards">
+                <div className="card">
+                    <img src={mask} alt="" />    
+                    <h3>Tezda xizmat ko’rsatish</h3>
+                    <p>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p> 
+
+                </div>
+
+                 <div className="card">
+                    <img src={mask} alt="" />    
+                    <h3>Tezda xizmat ko’rsatish</h3>
+                    <p>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p> 
+                           
+                </div>
+
+                 <div className="card">
+                    <img src={mask} alt="" />    
+                    <h3>Tezda xizmat ko’rsatish</h3>
+                    <p>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p> 
+                           
+                </div>
+
+                
+               </div>
              </div>
           </div>
        </div>
