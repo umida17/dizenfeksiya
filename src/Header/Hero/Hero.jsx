804e45d6f038466bd9506baf5099154fd91b7e7c
uy <a.normuraev20@gmail.com>
2026-05-12 23:11:02 +0500

    hero qismi bowlandi

--- src/Header/Hero/Hero.jsx
+++ src/Header/Hero/Hero.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import "./Hero.css"; 
 
 import group from "../../assets/Group.png";
+import vector from "../../assets/Vector.png"
+import xlor from "../../assets/xlor.png"
 const Hero = () => {
   return (
      <>
@@ -12,9 +14,16 @@ const Hero = () => {
                 <h2>Dizenfeksiya xizmati </h2>
                 <p>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p>
                 <div className="dumaloq">
+                    <div className="kok"></div>
+                    <div className="mentol"></div>
                     <img src={group} alt="" />
                 </div>
+                <div className="boshlanish">
+                    <button>Bog’lanish</button>
+                        <img src={vector} alt="" />
+                </div>
             </div>
+            <img src={xlor} alt="" />
         </div>
     </div>
      </div>
