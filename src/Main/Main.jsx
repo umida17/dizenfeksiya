f4c4b555f3b6b6f34fcf408f9e7125395772f212
uy <a.normuraev20@gmail.com>
2026-05-13 00:41:03 +0500

    main stil berilmoqda

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import "./Main.css"
 import mask from  '../assets/Mask.png'
+import xat from '../assets/xat.png'
+import maqul from '../assets/maqul.png'
+import xaworat from '../assets/xaworat.png'
 
 const Main = () => {
   return (
@@ -8,33 +11,45 @@ const Main = () => {
        <div className="section1">
           <div className="container">
              <div className="section1-containeri">
-               <h2>Avfzalliklarimiz</h2>
+               <h2 className='avf'>Avfzalliklarimiz</h2>
                <div className="cards">
                 <div className="card">
-                    <img src={mask} alt="" />    
-                    <h3>Tezda xizmat ko’rsatish</h3>
-                    <p>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p> 
+                    <img className='mas' src={mask} alt="" />    
+                    <h3 className='tezda'>Tezda xizmat ko’rsatish</h3>
+                    <p className='olib'>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p> 
+
+                </div>
+
+                <div className="card">
+                    <img className='mas' src={xat} alt="" />    
+                    <h3 className='tezda'>Yetuk mutahasislaimiz</h3>
+                    <p className='olib'>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p> 
 
                 </div>
 
                  <div className="card">
-                    <img src={mask} alt="" />    
-                    <h3>Tezda xizmat ko’rsatish</h3>
-                    <p>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p> 
-                           
+                    <img className='mas' src={maqul} alt="" />    
+                    <h3 className='tezda'>Sizga maqul vaqtda</h3>
+                    <p className='olib'>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p> 
+
                 </div>
 
-                 <div className="card">
-                    <img src={mask} alt="" />    
-                    <h3>Tezda xizmat ko’rsatish</h3>
-                    <p>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p> 
-                           
-                </div>
 
-                
                </div>
              </div>
           </div>
+       </div>
+
+       <div className="section2">
+        <div className="container">
+            <div className="section2-container">
+                <div className="klapa">
+                    <h2>Klapa va zararli hashorot endi yo’q deb hisoblang !!!</h2>
+                    <p>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p>
+                    <button>Bog’lanish</button>
+                </div>
+            </div>
+        </div>
        </div>
     </div>
   )
