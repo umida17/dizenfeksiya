32a03fd85d699e70baa5610918e57c1b4ec9b53d
uy <a.normuraev20@gmail.com>
2026-05-13 00:01:59 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css"
 import React from 'react'
 import Header from "./Header/Header"
 import Hero from "./Header/Hero/Hero";
-import Main from ".Main/Main"
+import Main from "./Main/Main"
 const App = () => {
   return (
     <div>
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -4,16 +4,13 @@ import "./Main.css"
 const Main = () => {
   return (
     <div>
-      <Main>
-        <div className="section1">
-            <div className="container">
-                <div className="section1-containeri">
-
-                    
-                </div>
-            </div>
-        </div>
-      </Main>
+       <div className="section1">
+          <div className="container">
+             <div className="section1-containeri">
+               
+             </div>
+          </div>
+       </div>
     </div>
   )
 }
