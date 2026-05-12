8fad738390da1fa70b1a04a38d14e3d9dbadc2c3
uy <a.normurzaev20@gmail.com>
2026-05-12 21:22:11 +0500

    hero qismi

---
+++ src/Header/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+     <>
+     <div className="Hero">
+    <div className="container">
+        <div className="Hero-container">
+            <div className="dizenfeksiya">
+                <h2>Dizenfeksiya xizmati </h2>
+                <p>Bizning ko’p yillardan beri o’z faoliyatini olib kelayotgan kompaniyamiz Mijozlarimiz bizdan mamnun</p>
+                
+            </div>
+        </div>
+    </div>
+     </div>
+     
+     </>
+  )
+}
+
+export default Hero
